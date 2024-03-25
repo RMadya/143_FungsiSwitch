d5904fed7909775f2830d8c066b717e6bcd87a8a
RMadya <kereyoben75@gmail.com>
2024-03-25 10:18:37 +0700

    input menu

--- calculator1/calculator1.cpp
+++ calculator1/calculator1.cpp
@@ -26,4 +26,22 @@ int pembagian(int bil_1, int bil_2) {
 	return bil_1 / bil_2;
 }
 
+int main() {
+	int pilihan;
+	do {
+		system("cls");
+		cout << "===========Menu==========" << endl;
+		cout << "1. Penjumlahan" << endl;
+		cout << "2. Pengurangan" << endl;
+		cout << "3. Perkalian" << endl;
+		cout << "4. Pembagian" << endl;
+		cout << "5. EXIT" << endl;
+		cout << "Masukkan menu yang ingin dipilih (1/2/3/4/5): ";
+		cin >> pilihan;
+
+		switch (pilihan) {
+		case 1: 
+		}
+	}
+}
 
